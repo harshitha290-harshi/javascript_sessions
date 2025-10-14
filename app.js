f0416e743fe89d642f927941662aa145d1069827
Harshitha <rharshitha290@gmail.com>
2025-10-14 23:53:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,144 @@
+//Assignment operator ........  it is used to asign the values
+let a;
+let b=55;
+// a=b
+// console.log(a);
+
+b=a
+console.log(a);
+
+let a1=10
+let b1 =55
+a=b
+ console.log(a1);
+ console.log(b1);
+
+//Arithmitic operator............values or numbers
+//+ operator
+console.log(2+3);
+//- operator
+console.log(22-3);
+//* operator
+console.log(3*6);
+// division
+console.log(22/2);
+// modules      reminder
+console.log(30%23);
+//power **
+console.log(3**2);
+
+let c=10
+let d=10
+console.log(c+d);
+console.log(c-d);
+console.log(c*d);
+console.log(c/d);
+console.log(c%d);
+console.log(c**d);
+
+//conditional opeartors //reational operators //comparision operators
+   /* >,<,>=,<=,!= not equal,!== strict not equal,==,=== */
+let g=10
+let h=20
+console.log(g>h);
+console.log(g<h);
+console.log(g>=h);
+console.log(g<=h);
+console.log(5==5);
+console.log('5'=='5');
+console.log("5"==5);//false
+console.log(5===5);
+console.log("5"===5);
+console.log("5"==="5");
+console.log("50"!=40); //not equal
+console.log("5"!==5);
+
+//short hand math
+let ab=10;
+ab=ab+20;
+console.log(ab);//30
+            ab=ab+20//50
+            ab+=20; //70
+            console.log(ab);
+ ab-=10;//60
+ console.log(ab);
+ 
+//unary operator
+let num1=10
+num1= num1+1; //11
+num1+=1;       //12
+num1++;       //13
+console.log(num1);
+num1--;  //12
+console.log(num1);
+
+//ternary opeartor
+/*SYNTAX 
+(condition)?(true statement):(false statement);*/
+let student = 45;
+(student>80)?console.log("students present"):console.log("students absent");
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
